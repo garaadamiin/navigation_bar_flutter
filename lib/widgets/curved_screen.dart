fef01d98fc075d6d80f4ee57032d2688d3cc54de
Garaad Amiin <garaadamiin3@gmail.com>
2023-01-12 21:13:18 +0300

    curved navigation

---
+++ lib/widgets/curved_screen.dart
@@ -0,0 +1,37 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:flutter/material.dart';
+
+class CurvedScreen extends StatefulWidget {
+  const CurvedScreen({super.key});
+
+  @override
+  State<CurvedScreen> createState() => _CurvedScreenState();
+}
+
+class _CurvedScreenState extends State<CurvedScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepPurple,
+      bottomNavigationBar: CurvedNavigationBar(
+        backgroundColor: Colors.deepPurple,
+        color: Colors.deepPurple.shade200,
+        animationDuration: const Duration(milliseconds: 300),
+        items: const [
+          Icon(
+            Icons.home,
+            color: Colors.white,
+          ),
+          Icon(
+            Icons.favorite,
+            color: Colors.white,
+          ),
+          Icon(
+            Icons.settings,
+            color: Colors.white,
+          ),
+        ],
+      ),
+    );
+  }
+}
