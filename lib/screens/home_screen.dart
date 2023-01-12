e4c3415e8e59aaaf660ab14727eb5ed2125ffb86
Garaad Amiin <garaadamiin3@gmail.com>
2023-01-12 21:12:35 +0300

    google and curved navigation

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,59 +1,12 @@
-import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
-import 'package:google_nav_bar/google_nav_bar.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.deepPurple,
-      bottomNavigationBar: CurvedNavigationBar(
-        backgroundColor: Colors.deepPurple,
-        color: Colors.deepPurple.shade200,
-        animationDuration: Duration(milliseconds: 300),
-        items: [
-          Icon(
-            Icons.home,
-            color: Colors.white,
-          ),
-          Icon(
-            Icons.favorite,
-            color: Colors.white,
-          ),
-          Icon(
-            Icons.settings,
-            color: Colors.white,
-          ),
-        ],
-      ),
-
-      //   bottomNavigationBar: GNav(
-      //   tabs: const [
-      //     GButton(
-      //       icon: Icons.home,
-      //       text: 'Home',
-      //     ),
-      //     GButton(
-      //       icon: Icons.favorite,
-      //       text: 'Favorite',
-      //     ),
-      //     GButton(
-      //       icon: Icons.search,
-      //       text: 'Search1',
-      //     ),
-      //     GButton(
-      //       icon: Icons.settings,
-      //       text: 'Settings',
-      //     ),
-      //   ],
-      // ),
+    return const Center(
+      child: Text('Home'),
     );
   }
 }
